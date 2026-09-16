a37f830e607d9515093f81327359e97ff5fe7f65
Radhesh-Roy <radheshroy0011@gmail.com>
2026-09-16 23:40:34 +0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
-
       title: 'Task Manager',
-
       home: TaskView(),
     );
   }
