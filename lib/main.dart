975d0eb13d9c6c65089e274351c1541318f6419f
Radhesh-Roy <radheshroy0011@gmail.com>
2026-09-26 21:23:42 +0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:simple_task/task_provider/task_provider.dart';
 import 'package:simple_task/view-page/task_view.dart';
 void main() {
   runApp(
+
     MultiProvider(
       providers: [
         ChangeNotifierProvider(
